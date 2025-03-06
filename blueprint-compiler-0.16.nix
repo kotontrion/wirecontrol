62e940c2ac7d1e4dd4b9d4c7e0e404ba519cf9c5
kotontrion <kotontrion@tutanota.de>
2025-03-06 15:30:01 +0100

    blueprint-compiler-0.16 skip docs

--- blueprint-compiler-0.16.nix
+++ blueprint-compiler-0.16.nix
@@ -1,13 +1,16 @@
 
-self: super: {
-  blueprint-compiler = super.blueprint-compiler.overrideAttrs (oldAttrs: {
+self: prev: {
+  blueprint-compiler = prev.blueprint-compiler.overrideAttrs (oldAttrs: {
     version = "0.16.0";
-    src = super.fetchFromGitLab {
+    src = prev.fetchFromGitLab {
       domain = "gitlab.gnome.org";
       owner = "jwestman";
       repo = "blueprint-compiler";
       rev = "v0.16.0";
       sha256 = "1y40kf9yfrjlfr5ax27j7ksv27fsznl7jhvvkzbfifdymjv10wqn";
     };
+    patches = (prev.patches or []) ++ [
+      ./skip-docs.patch
+    ];
   });
 }
